f7083e4e7b3a1d3f1288333e706a9e8641ea7621
RimvydasVainutisCS <97608500+RimvydasVainutisCS@users.noreply.github.com>
2022-04-12 11:47:29 +0300

    WebElement > IWebElement

--- TestTask20/NewsPage.cs
+++ TestTask20/NewsPage.cs
@@ -25,25 +25,25 @@ namespace TestTask20
         public void TestLogin()
         {
             driver.Navigate().GoToUrl("https://www.bbc.com/");
-            WebElement consentAccept = (WebElement)driver.FindElement(By.XPath("//div[1]/div[2]/div[2]/button[1]/p"));
+            IWebElement consentAccept = driver.FindElement(By.XPath("//div[1]/div[2]/div[2]/button[1]/p"));
             consentAccept.Click();
             Thread.Sleep(1000);
-            WebElement agreeCookies = (WebElement)driver.FindElement(By.CssSelector("#bbccookies-continue-button > span:nth-child(2)"));
+            IWebElement agreeCookies = driver.FindElement(By.CssSelector("#bbccookies-continue-button > span:nth-child(2)"));
             agreeCookies.Click();
             Thread.Sleep(1000);
-            WebElement yourAccountButton = (WebElement)driver.FindElement(By.CssSelector("#idcta-link"));
+            IWebElement yourAccountButton = driver.FindElement(By.CssSelector("#idcta-link"));
             yourAccountButton.Click();
             Thread.Sleep(1000);
-            WebElement userEmail = (WebElement)driver.FindElement(By.CssSelector("#user-identifier-input"));
+            IWebElement userEmail = driver.FindElement(By.CssSelector("#user-identifier-input"));
             userEmail.SendKeys("nurtohitra@vusra.com");
             Thread.Sleep(1000);
-            WebElement userPassword = (WebElement)driver.FindElement(By.CssSelector("#password-input"));
+            IWebElement userPassword = driver.FindElement(By.CssSelector("#password-input"));
             userPassword.SendKeys("testingLearning2022");
             Thread.Sleep(1000);
-            WebElement submitButton = (WebElement)driver.FindElement(By.CssSelector("#submit-button"));
+            IWebElement submitButton = driver.FindElement(By.CssSelector("#submit-button"));
             submitButton.Click();
             Thread.Sleep(1000);
-            WebElement yourAccount = (WebElement)driver.FindElement(By.CssSelector("#idcta-username"));
+            IWebElement yourAccount = driver.FindElement(By.CssSelector("#idcta-username"));
             Assert.AreEqual(yourAccount.Text, "Your account");
         }
 
